6da55a680faefba74175ee4da77b68a29b54bd7f
leosakaguti <sakaguti.leo@gmail.com>
2023-10-19 20:49:17 -0300

    Alteração na paleta de cores. Imagem trocada por animação. Página de topologia avançada.

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+
+container: document.getElementById('topologia-animation'),
+
+path: 'topologia-animation.json',
+
+renderer: 'svg',
+
+loop: true,
+
+autoplay: true,
+
+name: "Demo Animation",
+
+});
